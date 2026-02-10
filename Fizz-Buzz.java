332b15dd04d132f49122f8d0f6ee77bb39244313
Yatender Singh <168052424+YatenderSingh15@users.noreply.github.com>
2026-02-10 21:25:40 +0530

    [412] [Time Beats: 99.64%] [Memory Beats: 44.33%] - LeetPush

---
+++ Fizz-Buzz.java
@@ -0,0 +1,20 @@
+1class Solution {
+2    public List<String> fizzBuzz(int n) {
+3        List<String> answer=new ArrayList<>();
+4        for(int i=1; i<= n; i++){
+5            if(i % 3==0 && i % 5==0){
+6                answer.add("FizzBuzz");
+7            }
+8            else if(i % 3==0){
+9                answer.add("Fizz");
+10            }
+11            else if(i % 5==0){
+12                answer.add("Buzz");
+13            }
+14            else{
+15                answer.add(String.valueOf(i));
+16            }
+17        }
+18        return answer;
+19    }
+20}
