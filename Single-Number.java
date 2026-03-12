000da9d98d1c4b0ae47739c0c8e472ee11f8cdcb
Yatender Singh <168052424+YatenderSingh15@users.noreply.github.com>
2026-03-13 00:19:21 +0530

    [136] [Time Beats: 99.95%] [Memory Beats: 62.30%] - LeetPush

--- Single-Number.java
+++ Single-Number.java
@@ -1,14 +1,9 @@
 1class Solution {
 2    public int singleNumber(int[] nums) {
-3        HashMap<Integer,Integer> map = new HashMap<>();
+3        int xor = 0;
 4        for(int num : nums){
-5            map.put(num,map.getOrDefault(num,0)+1);
+5            xor = xor ^ num;
 6        }
-7        for(int key : map.keySet()){
-8            if(map.get(key) == 1){
-9                return key;
-10            }
-11        }
-12        return -1;
-13    }
-14}
+7        return xor;
+8    }
+9}
