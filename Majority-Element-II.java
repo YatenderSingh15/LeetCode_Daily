9638cd59e3caf75ca586d3beb436e46389d870a6
Yatender Singh <168052424+YatenderSingh15@users.noreply.github.com>
2026-03-30 10:46:19 +0530

    [229] [Time Beats: 43.33%] [Memory Beats: 20.61%] - LeetPush

--- Majority-Element-II.java
+++ Majority-Element-II.java
@@ -1,19 +1,15 @@
 1class Solution {
 2    public List<Integer> majorityElement(int[] nums) {
-3        List<Integer> ans = new ArrayList<>();
-4        for(int i =0; i <nums.length; i++){
-5            if(ans.contains(nums[i]))continue;
-6
-7            int count = 0;
-8            for(int j = 0; j < nums.length; j++){
-9                if(nums[i] == nums[j]){
-10                    count++;
-11                }
-12            }
-13            if(count > nums.length/3){
-14                ans.add(nums[i]);
-15            }
-16        }
-17        return ans;
-18    }
-19}
+3        HashMap<Integer,Integer> map = new HashMap<>();
+4        ArrayList<Integer> ans = new ArrayList<>();
+5        for(int num : nums){
+6            map.put(num,map.getOrDefault(num,0)+1);
+7        }
+8        for(int key : map.keySet()){
+9            if(map.get(key) > nums.length/3){
+10                ans.add(key);
+11            }
+12        }
+13        return ans;
+14    }
+15}
