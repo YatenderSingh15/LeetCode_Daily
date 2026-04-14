bb8796fcfbccc0cb430bd36b720be099b8270fc9
Yatender Singh <168052424+YatenderSingh15@users.noreply.github.com>
2026-04-14 11:00:27 +0530

    [2697] [Time Beats: 99.83%] [Memory Beats: 47.59%] - LeetPush

--- Lexicographically-Smallest-Palindrome.java
+++ Lexicographically-Smallest-Palindrome.java
@@ -1,9 +1,16 @@
 1class Solution {
 2    public String makeSmallestPalindrome(String s) {
-3        StringBuilder sb = new StringBuilder();
-4        for(int i = 0; i < s.length(); i++){
-5            sb.append(Character.toString(Math.min(s.charAt(i), s.charAt(s.length() - i - 1))));
-6        }
-7        return sb.toString();
-8    }
-9}
+3        char[] a = s.toCharArray();
+4        int left = 0;
+5        int right = a.length - 1;
+6        while (left < right) {
+7            if (a[left] != a[right]) {
+8                char smaller = (char) Math.min(a[left], a[right]);
+9                a[left] = a[right] = smaller;
+10            }
+11            left++;
+12            right--;
+13        }
+14        return new String(a);
+15    }
+16}
