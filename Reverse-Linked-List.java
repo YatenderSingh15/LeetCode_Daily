c01f3e16cf3bfad7ec68104899783019f0bd4e4e
Yatender Singh <168052424+YatenderSingh15@users.noreply.github.com>
2026-06-23 05:46:28 +0530

    [206] [Time Beats: 100.00%] [Memory Beats: 30.15%] - LeetPush

--- Reverse-Linked-List.java
+++ Reverse-Linked-List.java
@@ -10,19 +10,21 @@
 10 */
 11class Solution {
 12    public ListNode reverseList(ListNode head) {
-13        ListNode prev = null;
-14        ListNode present = head;
-15        
-16
-17        while(present != null){
-18            ListNode next = present.next;
-19            present.next = prev;
-20            prev = present;
-21            present = next;
-22            if(next != null){
-23                next = next.next;
-24            }
-25        }
-26        return prev; 
-27    }
-28}
+13        if(head == null){
+14            return head;
+15        }
+16        ListNode prev = null;
+17        ListNode present = head;
+18        ListNode next = present.next;
+19
+20        while(present != null){
+21            present.next = prev;
+22            prev = present;
+23            present = next;
+24            if(next != null){
+25                next = next.next;
+26            }
+27        }
+28        return prev; 
+29    }
+30}
