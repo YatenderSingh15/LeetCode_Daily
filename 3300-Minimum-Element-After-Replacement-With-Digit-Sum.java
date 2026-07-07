236fd0f2bca4378edab26ec90880dca6b32f7f66
Yatender Singh <168052424+YatenderSingh15@users.noreply.github.com>
2026-07-07 12:28:53 +0530

    [3300] [Time Beats: 99.99%] [Memory Beats: 19.27%] - LeetPush

--- 3300-Minimum-Element-After-Replacement-With-Digit-Sum.java
+++ 3300-Minimum-Element-After-Replacement-With-Digit-Sum.java
@@ -1,16 +1,15 @@
 class Solution {
     public int minElement(int[] nums) {
-        for(int i = 0; i < nums.length; i++){
-            int num = nums[i];
-            int rem = 0;
+        int min = Integer.MAX_VALUE;
+        for(int num : nums){
+            int sum = 0;
             while(num > 0){
-                rem = rem + num % 10;
+                sum = sum + num % 10;
                 num = num/10;
             }
-            nums[i] = rem;
+            num = sum;
+            min = Math.min(min,num);
         }
-        Arrays.sort(nums);
-        int min = nums[0];
         return min;
     }
 }
