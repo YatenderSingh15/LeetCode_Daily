6e89faaae967be1b1e64022a478b155a2e683af6
Yatender Singh <168052424+YatenderSingh15@users.noreply.github.com>
2026-07-15 09:09:38 +0530

    [3658] [Time Beats: 83.11%] [Memory Beats: 6.12%] - LeetPush

--- 3658-GCD-of-Odd-and-Even-Sums.java
+++ 3658-GCD-of-Odd-and-Even-Sums.java
@@ -1,5 +1,12 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        return n;
+        int oddSum = n*n;
+        int evenSum = n *(n+1);
+        while(oddSum != 0){
+            int rem = evenSum % oddSum;
+            evenSum = oddSum;
+            oddSum = rem;
+        }
+        return evenSum;
     }
 }
